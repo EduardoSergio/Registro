097937cf2c5f13b97c2053a4b7c5d23c530a04ab
EduardoSergio <aboutmaestro@hotmail.com>
2018-02-19 22:19:53 -0600

    Bienvenida

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php>
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 1.0 Stict//EN"
+	"http://www.w3.org/TR/xhtml" xml: lang= "en" lang= "en">
+<head>
+<title> Bienvenida <\title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<\head>
+
+<body>
+<br/>
+<h2> Nuestra misión, visión y valores. </h2>
+<p> 
+Misión
+Satisfacer a nuestros clientes a través de bebidas que generan bienestar.
+</p>
+<br><br/>
+
+<p>
+Visión
+Ser la empresa líder en México, reconocida por la calidad, excelencia operativa y continua innovación, impulsada por el talento de nuestra gente, guiada por un modelo de negocio que comparte el riesgo y la rentabilidad.
+</p>
+
+<br><br/>
+
+<h2><p>
+Valores
+Adaptación a los cambios:
+Somos flexibles y estamos preparados para afrontar retos, brindando a nuestros colaboradores y clientes un entorno estimulante y motivador.
+Excelencia y Productividad:
+Trabajamos de forma eficaz y eficiente, agregando valor en cada actividad, para garantizar productos y servicios de excelencia, a nuestros clientes, consumidores y sociedad.
+Integridad:
+Somos congruentes con lo que pensamos, decimos y hacemos, buscando siempre el bien común, apegándonos a la verdad, a la justicia y a nuestros valores
+Responsabilidad Social y Ambiental:
+Estamos comprometidos con el bienestar de nuestros colaboradores y familias, de nuestros consumidores y de las comunidades donde operamos, siempre respetando los derechos humanos, la legislación vigente y el medio ambiente.
+</p></h2>
+<br><br/>
+
+</body>
+
+</html>
---
+++ registro.php
@@ -0,0 +1,23 @@
+<?php>
+?>
+
+<head>
+<title> Registro de Usuarios </title>
+</head>
+
+<body>
+<p> Registra los siguientes datos solicitados: </p>
+
+<form method="post">
+
+<p>Nombre: <input type="text" name="nombre"/> <\p>
+<p>ApellidoPaterno: <input type="text" name="ApellidoPaterno" /></p>
+<p>ApellidoMaterno: <iput type="text" name="ApellidoMaterno" /></p>
+<p>Teléfono: <iput type="text" name="Telefono" /></p>
+<p>Email: <iput type="text" name="Email" /></p>
+
+</form>
+
+</body>
+
+</html>
